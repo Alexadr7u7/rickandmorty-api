5fee3649305a53bb4508ea6cb0de3826b7328a7a
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2025-07-19 20:00:36 -0600

    personajes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Characters } from './pages/characters/characters';
+import { Episodes } from './pages/episodes/episodes';
+import { Locations } from './pages/locations/locations';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'personajes', component: Characters },
+  { path: 'episodios', component: Episodes },
+  { path: 'lugares', component: Locations },
+  { path: '**', redirectTo: '' }, // Ruta de fallback
+];
--- src/app/pages/characters/characters.ts
+++ src/app/pages/characters/characters.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar';
+import { MainCharacters } from '../home/main-characters/main-characters';
 
 @Component({
   selector: 'app-characters',
-  imports: [],
+  imports: [Navbar, MainCharacters],
   templateUrl: './characters.html',
-  styleUrl: './characters.css'
+  styleUrl: './characters.css',
 })
-export class Characters {
-
-}
+export class Characters {}
