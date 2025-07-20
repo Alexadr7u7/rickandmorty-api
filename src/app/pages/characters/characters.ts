286ec9b5ab5736e7dfcb99619128fb67e2bb022c
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2025-07-19 21:42:33 -0600

    filtros ui

--- src/app/pages/characters/characters.ts
+++ src/app/pages/characters/characters.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar';
 import { MainCharacters } from '../home/main-characters/main-characters';
+import { Hero } from './hero/hero';
 
 @Component({
   selector: 'app-characters',
-  imports: [Navbar, MainCharacters],
+  imports: [Navbar, MainCharacters, Hero],
   templateUrl: './characters.html',
   styleUrl: './characters.css',
 })
